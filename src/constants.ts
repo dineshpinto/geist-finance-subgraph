83b93a88ef214679184784d1c2c029b75e49d4ab
dineshpinto <dkpinto95@gmail.com>
2022-03-08 16:56:54 +0100

    Added ability to handle interactions

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,6 @@
-import { Address } from "@graphprotocol/graph-ts";
+import { BigInt, BigDecimal, Address } from "@graphprotocol/graph-ts";
+
+export const ADDRESS_ZERO = Address.fromString("0x0000000000000000000000000000000000000000")
 
 export const TOKEN_ADDRESS = Address.fromString("0xd8321AA83Fb0a4ECd6348D4577431310A6E0814d");
 export const TOKEN_DECIMALS: i32 = 18; 
@@ -9,3 +11,9 @@ export const REWARD_TOKEN_ADDRESS = Address.fromString("0xd8321AA83Fb0a4ECd6348D
 export const REWARD_TOKEN_DECIMALS: i32 = 18; 
 export const REWARD_TOKEN_NAME: string = "Geist.Finance Protocol Token"; 
 export const REWARD_TOKEN_SYMBOL: string = "GEIST"; 
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+
+export let ZERO_BD = BigDecimal.fromString("0")
+export let ONE_BD = BigDecimal.fromString("1")
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,10 +1,51 @@
+import { BigDecimal, BigInt, Address, ethereum } from '@graphprotocol/graph-ts'
+
 import { Approval } from "../generated/GeistToken/GeistToken"
+import { DepositETHCall, BorrowETHCall, RepayETHCall, WithdrawETHCall } from "../generated/WETHGateway/WETHGateway"
+
 import { TOKEN_ADDRESS, REWARD_TOKEN_ADDRESS } from "./constants";
 import { getTokenInfo, getRewardTokenInfo } from './helpers';
-import { Token, RewardToken } from "../generated/schema"
+import { Token, RewardToken, UsageMetricsDailySnapshot} from "../generated/schema"
 
 
 export function handleApproval(event: Approval): void {
   let token: Token = getTokenInfo(TOKEN_ADDRESS);
   let rewardToken: RewardToken = getRewardTokenInfo(REWARD_TOKEN_ADDRESS, "DEPOSIT");
+}
+
+export function handleInteraction(block_number: BigInt, timestamp: BigInt): void {
+  let id: i64 = timestamp.toI64() / 60 / 60 / 24;
+
+  let usage_metrics = UsageMetricsDailySnapshot.load(id.toString());
+
+  if (!usage_metrics) {
+    usage_metrics = new UsageMetricsDailySnapshot(id.toString());
+    usage_metrics.activeUsers = 0;
+    usage_metrics.totalUniqueUsers = 0;
+    usage_metrics.dailyTransactionCount = 0;
+  }
+
+  usage_metrics.protocol = "geist-finance";
+  usage_metrics.activeUsers += 1;
+  usage_metrics.totalUniqueUsers += 1;
+  usage_metrics.dailyTransactionCount += 1
+  usage_metrics.blockNumber = block_number;
+  usage_metrics.timestamp = timestamp;
+  usage_metrics.save()
+}
+
+export function handleDepositETH(call: DepositETHCall): void {
+  handleInteraction(call.block.number, call.block.timestamp);
+}
+
+export function handleBorrowETH(call: BorrowETHCall): void {
+  handleInteraction(call.block.number, call.block.timestamp);
+}
+
+export function handleRepayETH(call: RepayETHCall): void {
+  handleInteraction(call.block.number, call.block.timestamp);
+}
+
+export function handleWithdrawETH(call: RepayETHCall): void {
+  handleInteraction(call.block.number, call.block.timestamp);
 }
