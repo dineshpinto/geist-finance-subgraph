2c3172b0724d9fa3310d9e12372be6e1b5ed0a72
dineshpinto <dkpinto95@gmail.com>
2022-03-10 07:51:48 +0100

    Moved handleInteraction to helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,7 +3,8 @@ import {
     Entity, 
     Value,
     ValueKind,
-    store
+    store,
+    BigInt
 } from '@graphprotocol/graph-ts'
 
 import { GeistToken as TokenContract } from "../generated/GeistToken/GeistToken"
@@ -16,7 +17,7 @@ import {
     REWARD_TOKEN_SYMBOL, 
 } from "./constants";
 
-import { Token, RewardToken } from "../generated/schema"
+import { Token, RewardToken, UsageMetricsDailySnapshot } from "../generated/schema"
 
 
 export function getTokenInfo(address: Address): Token {
@@ -32,16 +33,19 @@ export function getTokenInfo(address: Address): Token {
     let tokenContract = TokenContract.bind(address);
 
     token.id = address.toHexString();
-    token.decimals = tokenContract.try_decimals().reverted ? TOKEN_DECIMALS: tokenContract.try_decimals().value.toI32();
-    token.name = tokenContract.try_name().reverted ? TOKEN_NAME: tokenContract.try_name().value.toString();
-    token.symbol = tokenContract.try_name().reverted ? TOKEN_NAME: tokenContract.try_name().value.toString();
+    token.decimals = tokenContract.try_decimals().reverted ? 
+                     TOKEN_DECIMALS: tokenContract.try_decimals().value.toI32();
+    token.name = tokenContract.try_name().reverted ? 
+                 TOKEN_NAME: tokenContract.try_name().value.toString();
+    token.symbol = tokenContract.try_name().reverted ? 
+                   TOKEN_NAME: tokenContract.try_name().value.toString();
 
     token.save()
   
     return token;
 }
 
-export function getRewardTokenInfo(address: Address, rewardType: string): RewardToken {
+export function getRewardTokenInfo(address: Address): RewardToken {
     let rewardToken = RewardToken.load(address.toHexString());
        
     if (rewardToken) {
@@ -52,11 +56,13 @@ export function getRewardTokenInfo(address: Address, rewardType: string): Reward
 
     let tokenContract = TokenContract.bind(address);
     rewardToken.id = address.toHexString();
-    rewardToken.decimals = tokenContract.try_decimals().reverted ? REWARD_TOKEN_DECIMALS: tokenContract.try_decimals().value.toI32();
-    rewardToken.name = tokenContract.try_name().reverted ? REWARD_TOKEN_NAME: tokenContract.try_name().value.toString();
-    rewardToken.symbol = tokenContract.try_symbol().reverted ? REWARD_TOKEN_SYMBOL: tokenContract.try_symbol().value.toString();
-    rewardToken.type = rewardType;
-    
+    rewardToken.decimals = tokenContract.try_decimals().reverted ? 
+                           REWARD_TOKEN_DECIMALS: tokenContract.try_decimals().value.toI32();
+    rewardToken.name = tokenContract.try_name().reverted ? 
+                       REWARD_TOKEN_NAME: tokenContract.try_name().value.toString();
+    rewardToken.symbol = tokenContract.try_symbol().reverted ? 
+                         REWARD_TOKEN_SYMBOL: tokenContract.try_symbol().value.toString();
+    rewardToken.type = "DEPOSIT";
     rewardToken.save()
   
     return rewardToken;
@@ -90,3 +96,50 @@ export class Users extends Entity {
         );
     }
 }
+
+
+export function handleInteraction(
+    block_number: BigInt, 
+    timestamp: BigInt, 
+    from: Address
+    ): UsageMetricsDailySnapshot {
+    // Number of days since Unix epoch
+    // Note: This is an unsafe cast to int, this should be handled better, 
+    // perhaps some additional rounding logic
+    let id: i64 = timestamp.toI64() / 86400;
+  
+    // Check if the id (i.e. the day) exists in the store
+    let usageMetrics = UsageMetricsDailySnapshot.load(id.toString());
+  
+    // If the id does not exist, create it and reset values
+    if (!usageMetrics) {
+      usageMetrics = new UsageMetricsDailySnapshot(id.toString());
+      usageMetrics.activeUsers = 0;
+      usageMetrics.totalUniqueUsers = 0;
+      usageMetrics.dailyTransactionCount = 0;
+    }
+  
+    // Combine the id and the user address to generate a unique user id for the day
+    let userId: string = id.toString() + from.toString()
+    let userExists = Users.load(userId);
+  
+    // If the user id does not already exist in the store, add to unique users
+    if (!userExists) {
+      userExists = new Users(userId);
+      usageMetrics.activeUsers += 1;
+      usageMetrics.totalUniqueUsers += 1;
+    }
+  
+    // The protocol is defined in the schema as type Protocol!
+    // But doesnt this create a circular dependency?
+    // Protocol depends on usageMetrics, and usageMetrics depends on Protocol
+    usageMetrics.protocol = TOKEN_NAME;
+    usageMetrics.dailyTransactionCount += 1
+    usageMetrics.blockNumber = block_number;
+    usageMetrics.timestamp = timestamp;
+  
+    userExists.save();
+    usageMetrics.save();
+
+    return usageMetrics;
+  }
--- src/mapping.ts
+++ src/mapping.ts
@@ -1,8 +1,3 @@
-import { 
-  BigInt, 
-  Address, 
-} from '@graphprotocol/graph-ts'
-
 import { Approval } from "../generated/GeistToken/GeistToken"
 
 import { 
@@ -14,14 +9,13 @@ import {
 
 import { 
   TOKEN_ADDRESS,
-  TOKEN_NAME,
   REWARD_TOKEN_ADDRESS 
 } from "./constants";
 
 import { 
   getTokenInfo, 
   getRewardTokenInfo,
-  Users
+  handleInteraction,
 } from './helpers';
 
 import { 
@@ -34,61 +28,27 @@ import {
 export function handleApproval(event: Approval): void {
   // Currently set both the Token and Reward Token directly from constants
   let token: Token = getTokenInfo(TOKEN_ADDRESS);
-  let rewardToken: RewardToken = getRewardTokenInfo(REWARD_TOKEN_ADDRESS, "DEPOSIT");
-}
 
-export function handleInteraction(block_number: BigInt, timestamp: BigInt, from: Address): void {
-  // Number of days since Unix epoch
-  // Note: This is an unsafe cast to int, this should be handled better, 
-  // perhaps some additional rounding logic
-  let id: i64 = timestamp.toI64() / 86400;
-
-  // Check if the id (i.e. the day) exists in the store
-  let usageMetrics = UsageMetricsDailySnapshot.load(id.toString());
-
-  // If the id does not exist, create it and reset values
-  if (!usageMetrics) {
-    usageMetrics = new UsageMetricsDailySnapshot(id.toString());
-    usageMetrics.activeUsers = 0;
-    usageMetrics.totalUniqueUsers = 0;
-    usageMetrics.dailyTransactionCount = 0;
-  }
-
-  // Combine the id and the user address to generate a unique user id for the day
-  let userId: string = id.toString() + from.toString()
-  let userExists = Users.load(userId);
-
-  // If the user id does not already exist in the store, add to unique users
-  if (!userExists) {
-    userExists = new Users(userId);
-    usageMetrics.activeUsers += 1;
-    usageMetrics.totalUniqueUsers += 1;
-  }
-
-  // The protocol is defined in the schema as type Protocol!
-  // But doesnt this create a circular dependency?
-  // Protocol depends on usageMetrics, and usageMetrics depends on Protocol
-  usageMetrics.protocol = TOKEN_NAME;
-  usageMetrics.dailyTransactionCount += 1
-  usageMetrics.blockNumber = block_number;
-  usageMetrics.timestamp = timestamp;
-
-  userExists.save();
-  usageMetrics.save();
+  // Set rewardType of token to DEPOSIT
+  let rewardToken: RewardToken = getRewardTokenInfo(REWARD_TOKEN_ADDRESS);
 }
 
 export function handleDepositETH(call: DepositETHCall): void {
+  let usageMetrics: UsageMetricsDailySnapshot = 
   handleInteraction(call.block.number, call.block.timestamp, call.from);
 }
 
 export function handleBorrowETH(call: BorrowETHCall): void {
+  let usageMetrics: UsageMetricsDailySnapshot = 
   handleInteraction(call.block.number, call.block.timestamp, call.from);
 }
 
 export function handleRepayETH(call: RepayETHCall): void {
+  let usageMetrics: UsageMetricsDailySnapshot = 
   handleInteraction(call.block.number, call.block.timestamp, call.from);
 }
 
 export function handleWithdrawETH(call: WithdrawETHCall): void {
+  let usageMetrics: UsageMetricsDailySnapshot = 
   handleInteraction(call.block.number, call.block.timestamp, call.from);
 }
